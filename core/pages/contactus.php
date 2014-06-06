f1801ccd876fa0336378dd9bcd43be9e83579a62
Asif Javed <asif.javed@gmail.com>
2014-06-06 16:14:53 +0100

    V1.2 - Google Map Integration

--- core/pages/contactus.php
+++ core/pages/contactus.php
@@ -1,17 +1,24 @@
 <h1>Contact Us</h1>
 
-<!--div id="addressArea" class="addressArea">        	
-    <div class="right_container_pos" id="map_canvas"></div>            
+<div id="addressArea" class="addressArea">        	
+    <div class="right_container_pos" id="map-canvas"></div>           
+<input type="hidden" id="address" value="W2 1RH" />
+<input type="hidden" id="mapmarker" value="/images/mapmarker.png" />
     <div id="body_text">	        
         <p> Address goes here</p>
     </div>
 </div> 
-<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=true"></script-->
+<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=true"></script>
 
 <div id="contactFormArea">    
     <?php require_once CORE . '/inc/contact_form.php'; ?>    
 </div>
+<script>
+    $(document).ready(function() {
+        google.maps.event.addDomListener(window, 'load', initialize);
 
+    });
+</script>
 
 
 
--- public_html/js/global.js
+++ public_html/js/global.js
@@ -1,11 +1,67 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
+/******************************** Map Coding ********************************/
+var geocoder;
+var map;
+
+function initialize() {
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(-34.397, 150.644);
+    var mapOptions = {
+        zoom: 16,
+        center: latlng
+    }
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  
+	  codeAddress();
+}
+
+function codeAddress() {
+    var address = document.getElementById('address').value;
+    geocoder.geocode({'address': address}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+            var image = document.getElementById('mapmarker').value;
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location,
+                icon: image
+            });
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}
+
+/*********************************** Google Analytics *******************************/
+/*
+(function(b, o, i, l, e, r) {
+    b.GoogleAnalyticsObject = l;
+    b[l] || (b[l] =
+            function() {
+                (b[l].q = b[l].q || []).push(arguments)
+            });
+    b[l].l = +new Date;
+    e = o.createElement(i);
+    r = o.getElementsByTagName(i)[0];
+    e.src = '//www.google-analytics.com/analytics.js';
+    r.parentNode.insertBefore(e, r)
+}(window, document, 'script', 'ga'));
+ga('create', 'UA-XXXXX-X');
+ga('send', 'pageview');
+*/
+
+/*======================================================================================*/
+
+
+
+
+
+
 
 
 $(document).ready(function() {
     
+    /*********************** Slide Show ******************************/
                   
     $(".slideshow").cycle({
                      
@@ -22,16 +78,19 @@ $(document).ready(function() {
                    
     });                
        
-                
+    /************************* JCarousel *****************************/            
                 
     jQuery('#mycarousel').jcarousel({
         // Configuration goes here
         });
                 
+    /*********************** Colour Box Slide Show *******************/
     $(".slideshow3").colorbox({
         rel:'carouselImg', 
         slideshow:true
     });
+
+    /************************ colorbox popup ****************************/
                 
     //$(".iframe").colorbox({iframe:true, innerWidth:"820px", innerHeight:"780"});                    
     $(".iframe").colorbox({
@@ -40,6 +99,8 @@ $(document).ready(function() {
         innerHeight:780
     });                    
     
+
+    /************************ Contact form Validation **************************************/
     //DD_belatedPNG.fix('.pngfix');
 var validator = $('#frmContact').validate({ // initialize the plugin
 	//onkeyup: false, // stop validating on key press
@@ -119,6 +180,10 @@ var validator = $('#frmContact').validate({ // initialize the plugin
 		   return false; 
 		 }); 
 	   
+
+/********************************************************************/
+
+
 
 
 
